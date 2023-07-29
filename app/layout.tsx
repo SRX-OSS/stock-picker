0793066fd35d67351dd73a466c9c46666301832f
SRX9 <aquasr9@gmail.com>
2023-07-30 00:13:16 +0530

    metadata removal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,26 +4,8 @@ import "@/styles/globals.css";
 import { SiteHeader } from "@/components/site-header";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { ThemeProvider } from "@/components/theme-provider";
-import { siteConfig } from "@/config/site";
 import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
-import { Metadata } from "next";
-
-// SEO MetaTags
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
 
 interface RootLayoutProps {
   children: React.ReactNode;
